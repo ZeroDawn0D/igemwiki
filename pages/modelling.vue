899fe5f1c9e855563e8b954bc3937201a40b1b9c
Mrigank Pawagi <mrigankpawagi@gmail.com>
2023-10-12 21:27:14 +0530

    modelling pdf

--- pages/modelling.vue
+++ pages/modelling.vue
@@ -8,18 +8,9 @@
     <!-- IMPORTANT CHANGE TO MAKE HERE-->
     <div class =  "w-3/4 max-md:w-screen  relative left-2/4 -translate-x-2/4">
       
-<div class = "text-4xl courierprimebold">
-PARTS SAFETY  
-</div>
-All components of our project have been recorded in our safety form. The proteins Interleukin 8 and O-IL8-15 were checked in with iGEM. Both proteins have undergone extensive review by the team’s Principal Investigators as well as the iGEM Safety and Security Committee before being approved for use in the laboratories.   
-<div class = "text-4xl courierprimebold">
-LAB SAFETY  
-</div>
-To ensure absolute safety of everyone involved in laboratory work, all team members were screened and certified by the Office of Laboratory Safety and Environmental Health (OLSEH) in IISc before commencement of wet lab procedures.  
+      <iframe src="https://static.igem.wiki/teams/4829/wiki/modelling/modelling.pdf" width="100%" height="800px"></iframe>
 
-Lab instructors Dr. Lavanya S. and Mr. HS Vinod accompanied us during each lab session and offered invaluable advice and supervision to ensure equipment health and overall safety.  
 
-All experiments and procedures were conducted in BSL-2 laboratories and proper lab attire like lab coats, fresh nitrile gloves and footwear was mandated for students and staff alike.  
     </div>
   </div>
 </template>
